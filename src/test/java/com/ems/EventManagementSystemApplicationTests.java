dfbec801e5da886f50f0761fad694d480564e5e1
Mamatha-Dilish-ML <MamathaDilish.B@Xoriant.com>
2026-01-29 14:05:21 +0530

    initial build test commit

--- src/test/java/com/ems/EventManagementSystemApplicationTests.java
+++ src/test/java/com/ems/EventManagementSystemApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.ems;
+package com.ems;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
